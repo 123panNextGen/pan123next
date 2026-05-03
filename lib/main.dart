f83dc8712f85c37d035822e0e33b2ef59ab1b409
AImixAE Nya Mocha <AImixAE@Outlook.com>
2026-05-03 20:00:27 +0800

    feat: 添加新字体, 适配移动端, 应用新图标

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:get/get.dart';
 import 'package:pan123next/common/app_session.dart';
 import 'package:pan123next/common/data/app.dart';
 import 'package:pan123next/common/data/user.dart';
 import 'package:pan123next/common/get_platform.dart';
-import 'package:window_manager/window_manager.dart';
 
 import 'app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   // 对桌面端标题栏自定义
   if (isDesktop()) {
     WidgetsFlutterBinding.ensureInitialized();
