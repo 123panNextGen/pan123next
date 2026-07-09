86a333b52fb5ec8e68e67ce6533804c4cd0eb2ec
AImixAE Nya Mocha <AImixAE@outlook.com>
2026-07-09 22:08:13 +0800

    refactor(file_list): 简化文件列表页面右键菜单实现
    1. 移除冗余的右键菜单显示封装方法和手势检测逻辑
    2. 保留FlyoutTarget基础结构用于后续菜单扩展
    3. 更新README中文件下载的功能描述文本
    4. 新增云信息页面占位组件CloudInfoView

---
+++ lib/pages/cloud/view.dart
@@ -0,0 +1,16 @@
+import 'package:fluent_ui/fluent_ui.dart' hide FluentIcons;
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+
+class CloudInfoView extends StatefulWidget {
+  const CloudInfoView({super.key});
+
+  @override
+  State<CloudInfoView> createState() => _CloudInfoViewState();
+}
+
+class _CloudInfoViewState extends State<CloudInfoView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
